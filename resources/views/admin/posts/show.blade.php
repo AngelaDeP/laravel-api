e5091dc09f79548c39154de8701030158258cbda
Angela De Pasquale <angeladepasquale_@libero.it>
2022-04-15 10:22:33 +0200

    first commit

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,24 @@
+@extends('admin.layouts.base')
+
+@section('content')
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+
+            <h1>View your own post!</h1>
+
+            <div><strong>Title: </strong> {{$post->title}}</div>
+                <div><strong>Content: </strong> {!! $post->content !!}</div>
+                <div><strong>Category: </strong> {{$post->category->name}}</div>
+                <div><strong>Slug: </strong> {{$post->slug}}</div>
+                <div>
+                    @foreach ($post->tags as $tag)
+                        <span class="badge badge-primary">{{$tag->name}}</span>
+                    @endforeach
+                </div>
+            </div>
+
+            <a href="{{route('admin.posts.index')}}" class="btn btn-primary">Go back</a>
+        </div>
+    </div>
+@endsection
